33bcee89456dc86e3c837491f309f80271a96dfe
P.Rithwik <25ag1a66b2@gmail.com>
2026-08-07 07:09:36 +0000

    java

---
+++ PriCls.java
@@ -0,0 +1,11 @@
+class PriCls{
+    final static boolean b = true;
+    final public static void document()
+    {
+        System.out.println("hello");
+    }
+    public static void main(String[] args){
+        System.out.println(b);
+        document();   
+    }   
+}
